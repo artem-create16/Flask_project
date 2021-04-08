07666c03d1443463854d8a121aada240eb241059
artem <artem@artem>
2021-04-09 01:01:15 +0300

    -_-

--- application/currency/controller.py
+++ application/currency/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, flash
 
 from .form import CurrencyForm
 from .utils import get_currency
@@ -8,8 +8,13 @@ def show_form():
     form = CurrencyForm()
     result = get_currency()
     if form.validate_on_submit():
-        choice = str(form.first_choice.data)
-        print(choice)
+        first_selection = request.form.get('select1')
+        second_selection = request.form.get('select2')
+
+        print(first_selection)
+        print(second_selection)
+    else:
+        print(form.first_choice.errors)
 
     return render_template('form_currency.html', html_form=form, option=result)
 
--- application/currency/utils.py
+++ application/currency/utils.py
@@ -3,15 +3,10 @@ import requests
 
 def get_currency():
     request = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
-    answer = request.json()
+    answer = request.json()['Valute']
     dict_with_currency = {}
-    valute = answer.get('Valute')
-    for i in valute.keys():
-        dict_with_currency[valute[i]['Name']] = valute[i]['Value']
-    # print(dict_with_currency)
-    # for i in dict_with_currency.keys():
-    #     print(i)
+    for i in answer.keys():
+        dict_with_currency[answer[i]['Name']] = answer[i]['Value']
+
     return dict_with_currency
 
-
-# get_the_current_value_of_the_currency()
